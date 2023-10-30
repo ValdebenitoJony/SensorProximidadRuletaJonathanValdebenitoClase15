fe851efddafc926dfd92d9c52f3ff309722f200c
valde <J.valdebenito24@alumnos.santotomas.cl>
2023-10-30 14:59:32 -0300

    Initial commit

---
+++ app/src/main/java/com/example/sensorproximidadruleta/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.sensorproximidadruleta;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageSwitcher;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+    ConstraintLayout cl;
+    TextView texto;
+    SensorManager sn;
+    Sensor sensor;
+    ImageView ruleta;
+    Random random;
+    int angulo;
+    boolean restart = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ruleta = findViewById(R.id.ruleta);
+        cl = findViewById(R.id.Caja);
+        sn = (SensorManager) getSystemService(SENSOR_SERVICE);
+        sensor = sn.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+        sn.registerListener((SensorEventListener) this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+        random = new Random();
+
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event){
+        String texto1 = String.valueOf(event.values[0]);
+        texto.setText(texto1);
+        float valor = Float.parseFloat(texto1);
+        if (valor == 0){
+            angulo = random.nextInt(3600) * 360;
+            RotateAnimation rotateAnimation = new RotateAnimation(0, angulo,
+            RotateAnimation.RELATIVE_TO_SELF, 0.5f,
+            RotateAnimation.RELATIVE_TO_SELF, 0.5f);
+            rotateAnimation.setFillAfter(true);
+            rotateAnimation.setDuration(3600);
+            rotateAnimation.setInterpolator(new AccelerateDecelerateInterpolator());
+            ruleta.startAnimation(rotateAnimation);
+            restart = false;
+        }else {
+            texto.setText("pasa tu mano pa jugar");
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i){
+
+    }
+}
